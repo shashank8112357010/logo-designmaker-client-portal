2ce2b5c4a1aa949e73e87d5e158fd8dad1e7c241
PrathamShamra1111 <sharmapratham223@gmail.com>
2024-06-24 19:03:27 +0530

    sidebar chnages

--- src/pages/dashboard/Sidebar.jsx
+++ src/pages/dashboard/Sidebar.jsx
@@ -1,31 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { HomeIcon, ClipboardDocumentListIcon, RectangleStackIcon, Cog6ToothIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
 
 const Sidebar = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const [activePath, setActivePath] = useState(location.pathname);
-
-    const handleLinkClick = (path) => {
-        setActivePath(path);
-    };
-
-    const imageClasses = (path) =>
-        `${activePath === path ? 'bg-primaryGreen' : 'bg-white'}`;
-
-    const linkClasses = (path) =>
-        `relative flex items-center space-x-5 h-10 w-full pl-4
-        ${activePath === path ? 'text-primaryGreen bg-black bg-opacity-15' : 'text-white'}`;
-
-    const pseudoClasses = (path) =>
-        `${activePath === path ? 'border-l-8 border-primaryGreen rounded-tr-lg rounded-br-lg' : ''}`;
+    const [helpdeskOpen, setHelpdeskOpen] = useState(location.pathname.includes('/dashboard/help'));
 
     useEffect(() => {
         setActivePath(location.pathname);
+        if (location.pathname.includes('/dashboard/help')) {
+            setHelpdeskOpen(true);
+        } else {
+            setHelpdeskOpen(false);
+        }
     }, [location]);
 
+    const handleLinkClick = (path) => {
+        setActivePath(path);
+        navigate(path);
+        setHelpdeskOpen(path.includes('/dashboard/help'));
+    };
+
+
+    const getLinkClasses = (path) => {
+        const isActive = activePath === path || (helpdeskOpen && path.includes('/dashboard/help'));
+        const baseClasses = `relative flex items-center space-x-5 h-10 w-full pl-4`;
+        const activeClasses = `text-primaryGreen bg-black bg-opacity-15`;
+        const inactiveClasses = `text-white`;
+
+        return `${baseClasses} ${isActive ? activeClasses : inactiveClasses}`;
+    };
+
+
+    const getPseudoClasses = (path) => {
+        const isActive = activePath === path || (helpdeskOpen && path.includes('/dashboard/help'));
+        return isActive ? 'border-l-8 border-primaryGreen rounded-tr-lg rounded-br-lg' : '';
+    };
+
     return (
-        <aside className="w-[16.7%] bg-primaryBlack fixed top-0  overflow-y-auto h-screen">
+        <aside className="w-[16.7%] bg-primaryBlack fixed top-0 overflow-y-auto h-screen">
             <div className='flex flex-col justify-center space-y-10 w-full'>
                 <div className='m-6'>
                     <img src="/img/Logo.png" className="h-10 w-64 mb-10" alt="Logo" />
@@ -33,57 +48,80 @@ const Sidebar = () => {
                 <nav className="space-y-6 flex flex-col items-baseline justify-center w-full">
                     <Link
                         to="/dashboard/overview"
-                        className={linkClasses('/dashboard/overview')}
+                        className={getLinkClasses('/dashboard/overview')}
                         onClick={() => handleLinkClick('/dashboard/overview')}
                     >
-                        <div className={`absolute left-0 h-full w-2 ${pseudoClasses('/dashboard/overview')} `}></div>
+                        <div className={`absolute left-0 h-full w-2 ${getPseudoClasses('/dashboard/overview')} `}></div>
                         <HomeIcon className="h-6 w-6" />
                         <span>Overview</span>
                     </Link>
                     <Link
-                    to="/dashboard/services"
-                        className={linkClasses('/dashboard/services')}
+                        to="/dashboard/services"
+                        className={getLinkClasses('/dashboard/services')}
                         onClick={() => handleLinkClick('/dashboard/services')}
                     >
-                        <div className={`absolute left-0 h-full w-2 ${pseudoClasses('/dashboard/services')} `}></div>
+                        <div className={`absolute left-0 h-full w-2 ${getPseudoClasses('/dashboard/services')} `}></div>
                         <RectangleStackIcon className="h-6 w-6" />
                         <span>Services</span>
                     </Link>
                     <Link
-                    to="/dashboard/files"
-                        className={linkClasses('/dashboard/files')}
+                        to="/dashboard/files"
+                        className={getLinkClasses('/dashboard/files')}
                         onClick={() => handleLinkClick('/dashboard/files')}
                     >
-                        <div className={`absolute left-0 h-full w-2 ${pseudoClasses('/dashboard/files')} `}></div>
+                        <div className={`absolute left-0 h-full w-2 ${getPseudoClasses('/dashboard/files')} `}></div>
                         <ClipboardDocumentListIcon className="h-6 w-6" />
                         <span>Files</span>
                     </Link>
                     <Link
                         to="/dashboard/transactions"
-                        className={linkClasses('/dashboard/transactions')}
+                        className={getLinkClasses('/dashboard/transactions')}
                         onClick={() => handleLinkClick('/dashboard/transactions')}
                     >
-                        <div className={`absolute left-0 h-full w-2 ${pseudoClasses('/dashboard/transactions')} `}></div>
+                        <div className={`absolute left-0 h-full w-2 ${getPseudoClasses('/dashboard/transactions')} `}></div>
                         <div className='pl-1 flex justify-between items-center gap-6'>
-                            <img src='/img/sidebar/right.png' alt='i' className={`h-4 w-4 ${imageClasses('/dashboard/transactions')} p-0.5`} />
+                            <img src='/img/sidebar/right.png' alt='i' className={`h-4 w-4 ${activePath === '/dashboard/transactions' ? 'bg-primaryGreen' : 'bg-white'} p-0.5`} />
                             <span>Transactions</span>
                         </div>
                     </Link>
+                    <div className="relative w-full">
+                        <button
+                            className={getLinkClasses('/dashboard/help')}
+                            onClick={() => handleLinkClick('/dashboard/help')}
+                        >
+                            <div className={`absolute left-0 h-full w-2 ${getPseudoClasses('/dashboard/help')}`}></div>
+                            <QuestionMarkCircleIcon className="h-6 w-6" />
+                           <div className='flex justify-between items-center gap-3'>
+                           <span>Helpdesk</span>
+                           {helpdeskOpen ? <img src='/img/sidebar/downArrowGreen.png' alt='' className="h-1.5 w-2 ml-auto mt-2" /> : <img src='/img/sidebar/downArrow.png' alt='' className="h-1.5 w-2 ml-auto mt-2" />}
+                           </div>
+                        </button>
+                        {helpdeskOpen && (
+                            <div className="w-full pl-20 mb-10 flex flex-col">
+                                <Link
+                                    to="/dashboard/help"
+                                    className={`${ activePath ==='/dashboard/help' ? 'text-primaryGreen': 'text-customGray'}`}
+                                    onClick={() => handleLinkClick('/dashboard/help')}
+                                >
+                                    <span>Tickets</span>
+                                </Link>
+                                <Link
+                                    to="/dashboard/help/refunds"
+                                    className={`${ activePath ==='/dashboard/help/refunds' ? 'text-primaryGreen': 'text-customGray'}`}
+                                    onClick={() => handleLinkClick('/dashboard/help/refunds')}
+                                >
+                                    <span>Refunds</span>
+                                </Link>
+
+                            </div>
+                        )}
+                    </div>
                     <Link
-                        to="/dashboard/help"
-                        className={linkClasses('/dashboard/help')}
-                        onClick={() => handleLinkClick('/dashboard/help')}
-                    >
-                        <div className={`absolute left-0 h-full w-2 ${pseudoClasses('/dashboard/help')} `}></div>
-                        <QuestionMarkCircleIcon className="h-6 w-6" />
-                        <span>Helpdesk</span>
-                    </Link>
-                    <Link
-                    to="/dashboard/settings"
-                        className={linkClasses('/dashboard/settings')}
+                        to="/dashboard/settings"
+                        className={getLinkClasses('/dashboard/settings')}
                         onClick={() => handleLinkClick('/dashboard/settings')}
                     >
-                        <div className={`absolute left-0 h-full w-2 ${pseudoClasses('/dashboard/settings')} `}></div>
+                        <div className={`absolute left-0 h-full w-2 ${getPseudoClasses('/dashboard/settings')} `}></div>
                         <Cog6ToothIcon className="h-6 w-6" />
                         <span>Settings</span>
                     </Link>
