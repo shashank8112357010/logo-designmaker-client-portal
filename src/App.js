448774add5fb44c1162caacb9c6bc77886d30341
Khushi Jain <khushij1011@gmail.com>
2024-07-16 18:56:39 +0530

    updated token route

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ axios.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.data.message === 'Token is not valid' && !originalRequest._retry) {
+    if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
         const newAccessToken = await refreshAuthLogic();
