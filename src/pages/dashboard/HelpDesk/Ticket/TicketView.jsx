9c911d02749db7a2d23c8f3ff9b24bf3918b46c7
Kartik-Yadav9 <Kartikyadav0999@gmail.com>
2024-07-23 19:48:52 +0530

    'transition'

--- src/pages/dashboard/HelpDesk/Ticket/TicketView.jsx
+++ src/pages/dashboard/HelpDesk/Ticket/TicketView.jsx
@@ -117,7 +117,7 @@ const TicketView = ({ ticketData, onBack }) => {
                             <div className='bg-customGray h-0.5 w-full rounded-full'></div>
                         </div>
                         <div className="mt-4">
-                            {ticketData?.replies?.map(reply => (
+                            { ticketData?.replies?.map(reply => (
                                 <div key={reply?._id} className="bg-secondaryBlack rounded mt-4">
                                     <div className="flex justify-between items-start">
                                         <div className="flex items-center space-x-2 text-white">
@@ -170,71 +170,73 @@ const TicketView = ({ ticketData, onBack }) => {
                     </div>
                 </div>
             </main>
-            {showReplyForm && (
-                <div className="fixed inset-0 left-[16.7%] top-24 flex items-center justify-center z-10">
-                    <div className="absolute inset-0 bg-customGray bg-opacity-10 h-full"></div>
-                    <div className="bg-secondaryBlack p-6 rounded-lg w-[90%] z-20">
-                        <h3 className="text-lg text-white font-semibold mb-4">Reply to Ticket #{ticketData?._id}</h3>
-                        <form onSubmit={handleReplySubmit}>
-                            <div className='w-5/6'>
-                                <div className='flex gap-8'>
-                                    <div className="mb-4 w-1/3">
-                                        <label className="block text-sm font-medium text-white">Ticket Number</label>
-                                        <input
-                                            type="text"
-                                            value={ticketData?._id}
-                                            readOnly
-                                            className="mt-1 block w-full px-3 py-2 text-customGray rounded-md shadow-sm focus:outline-none bg-primaryBlack"
-                                        />
-                                    </div>
-                                    <div className="mb-4 w-1/3">
-                                        <label className="block text-sm font-medium text-white">Request Ticket Type</label>
-                                        <input
-                                            readOnly
-                                            type="text"
-                                            value={ticketData?.ticketType}
-                                            className="mt-1 block w-full px-3 py-2 text-customGray rounded-md shadow-sm focus:outline-none bg-primaryBlack"
-                                        />
-                                    </div>
-                                    <div className="mb-4 w-1/3">
-                                        <label className="block text-sm font-medium text-white">Priority</label>
-                                        <div className="mt-1 w-full px-3 py-2 flex items-center gap-2 text-customGray rounded-md shadow-sm bg-primaryBlack">
-                                            <div className={`w-3 h-3 rounded-full ${ticketData?.priorityStatus?.color}`}></div>
-                                            <span>{ticketData?.priorityStatus?.label}</span>
+            <div className={`${showReplyForm ? 'fade-enter-active' :  'fade-exit-active'}`}>
+                {showReplyForm && (
+                    <div className="fixed inset-0 left-[16.7%] top-24 flex items-center justify-center z-10">
+                        <div className="absolute inset-0 bg-customGray bg-opacity-10 h-full"></div>
+                        <div className="bg-secondaryBlack p-6 rounded-lg w-[90%] z-20">
+                            <h3 className="text-lg text-white font-semibold mb-4">Reply to Ticket #{ticketData?._id}</h3>
+                            <form onSubmit={handleReplySubmit}>
+                                <div className='w-5/6'>
+                                    <div className='flex gap-8'>
+                                        <div className="mb-4 w-1/3">
+                                            <label className="block text-sm font-medium text-white">Ticket Number</label>
+                                            <input
+                                                type="text"
+                                                value={ticketData?._id}
+                                                readOnly
+                                                className="mt-1 block w-full px-3 py-2 text-customGray rounded-md shadow-sm focus:outline-none bg-primaryBlack"
+                                            />
+                                        </div>
+                                        <div className="mb-4 w-1/3">
+                                            <label className="block text-sm font-medium text-white">Request Ticket Type</label>
+                                            <input
+                                                readOnly
+                                                type="text"
+                                                value={ticketData?.ticketType}
+                                                className="mt-1 block w-full px-3 py-2 text-customGray rounded-md shadow-sm focus:outline-none bg-primaryBlack"
+                                            />
+                                        </div>
+                                        <div className="mb-4 w-1/3">
+                                            <label className="block text-sm font-medium text-white">Priority</label>
+                                            <div className="mt-1 w-full px-3 py-2 flex items-center gap-2 text-customGray rounded-md shadow-sm bg-primaryBlack">
+                                                <div className={`w-3 h-3 rounded-full ${ticketData?.priorityStatus?.color}`}></div>
+                                                <span>{ticketData?.priorityStatus?.label}</span>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div className="mb-4">
+                                        <label className="block text-sm font-medium text-white">Ticket Body</label>
+                                        <textarea
+                                            placeholder='Type Your Reply here...'
+                                            value={replyText}
+                                            onChange={handleReplyChange}
+                                            required
+                                            className="placeholder:text-customGray mt-1 block w-full px-3 py-2 appearance-none resize-none bg-primaryBlack text-white rounded-md shadow-sm break-words"
+                                            rows="5"
+                                        ></textarea>
+                                    </div>
                                 </div>
-                                <div className="mb-4">
-                                    <label className="block text-sm font-medium text-white">Ticket Body</label>
-                                    <textarea
-                                        placeholder='Type Your Reply here...'
-                                        value={replyText}
-                                        onChange={handleReplyChange}
-                                        required
-                                        className="placeholder:text-customGray mt-1 block w-full px-3 py-2 appearance-none resize-none bg-primaryBlack text-white rounded-md shadow-sm break-words"
-                                        rows="5"
-                                    ></textarea>
-                                </div>
-                            </div>
 
-                            <div className="flex justify-end items-center space-x-4">
-                                {errorMessage && <div className="text-red-500 ">{errorMessage}</div>}
-                                <button
-                                    type="button"
-                                    onClick={handleCloseReplyForm}
-                                    className=" text-white py-2 px-8 border rounded-md border-primaryGreen">
-                                    Cancel
-                                </button>
-                                <button
-                                    type="submit"
-                                    className="bg-primaryGreen  text-primaryBlack font-medium rounded-md  py-2 px-2 w-36">
-                                    {loading ? <BeatLoader size={8} color={"#000"} /> : 'Submit Reply'}
-                                </button>
-                            </div>
-                        </form>
+                                <div className="flex justify-end items-center space-x-4">
+                                    {errorMessage && <div className="text-red-500 ">{errorMessage}</div>}
+                                    <button
+                                        type="button"
+                                        onClick={handleCloseReplyForm}
+                                        className=" text-white py-2 px-8 border rounded-md border-primaryGreen">
+                                        Cancel
+                                    </button>
+                                    <button
+                                        type="submit"
+                                        className="bg-primaryGreen  text-primaryBlack font-medium rounded-md  py-2 px-2 w-36">
+                                        {loading ? <BeatLoader size={8} color={"#000"} /> : 'Submit Reply'}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
            
         </div>
     );
